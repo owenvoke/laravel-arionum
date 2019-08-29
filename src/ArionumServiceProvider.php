3f301ac55a30e57ca8ec2bc667e0f5f7dd971ac8
Owen Voke <owzie123@gmail.com>
2019-08-29 13:16:33 +0100

    Add service provider and custom exception

---
+++ src/ArionumServiceProvider.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace pxgamer\LaravelArionum;
+
+use Illuminate\Support\ServiceProvider;
+use pxgamer\Arionum\Arionum as ArionumAdapter;
+use pxgamer\LaravelArionum\Exceptions\InvalidNodeUri;
+
+final class ArionumServiceProvider extends ServiceProvider
+{
+    private const CONFIG_FILE = __DIR__.'/../config/arionum.php';
+
+    public function boot(): void
+    {
+        $this->publishes([
+            self::CONFIG_FILE => $this->app->configPath('arionum.php'),
+        ], 'config');
+
+        $this->mergeConfigFrom(self::CONFIG_FILE, 'arionum');
+    }
+
+    public function register(): void
+    {
+        $this->app->singleton(Arionum::class, function () {
+            if (! $this->app->get('config')->get('arionum.node-uri')) {
+                throw InvalidNodeUri::environmentVariableNotSet();
+            }
+
+            return new ArionumAdapter($this->app->get('config')->get('arionum.node-uri'));
+        });
+
+        $this->app->alias(Arionum::class, 'arionum');
+    }
+}
